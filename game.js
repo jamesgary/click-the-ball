77018702b5c09a7004de3ab1fa4e8d77b2ed2c57
James Gary <mrjamesgary@gmail.com>
2014-08-14 02:29:26 -0700

    Final product

--- game.js
+++ game.js
@@ -1,103 +1,95 @@
-$(function() {
-  var canvas = document.getElementById("game");
-  var context = canvas.getContext("2d");
+var WIDTH = 400;
+var HEIGHT = 300;
 
-  var x = 200;
-  var y = 150;
-  var xSpeed = 0;
-  var ySpeed = 0;
-  var radius = 25;
+var container     = document.getElementById("container");
+var ballCanvas    = document.getElementById("game");
+var effectsCanvas = document.getElementById("game-effects");
 
-  canvas.addEventListener("click", function() {
-    var mouseX = event.x - canvas.offsetLeft;
-    var mouseY = event.y - canvas.offsetTop;
-    if (distance(x, y, mouseX, mouseY) < radius) {
-      flashText("YAY", mouseX, mouseY, 0, 255, 0)
-    } else {
-      flashText("BOO", mouseX, mouseY, 255, 0, 0)
-    }
-  });
+var ballCtx    = ballCanvas.getContext("2d");
+var effectsCtx = effectsCanvas.getContext("2d");
 
-  requestAnimationFrame(gameLoop);
+effectsCtx.font = "800 24pt Arial";
+effectsCtx.textAlign = "center";
 
-  function gameLoop() {
-    drawBall();
-    processGame();
-    requestAnimationFrame(gameLoop);
-  }
+var ballX = 200;
+var ballY = 150;
+var xSpeed = 0;
+var ySpeed = 0;
+var radius = 25;
+var texts = [];
 
-  function drawBall() {
-    context.fillStyle = "rgba(223, 239, 255, 0.1)";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
-    context.arc(x, y, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = 'green';
-    context.fill();
-    context.lineWidth = 2;
-    context.strokeStyle = '#030';
-    context.stroke();
-  }
-
-  function flashText(text, x, y, r, g, b) {
-    var startTime = Date.now();
-    context.font = "800 24pt Arial";
-    context.textAlign = "center";
-    var opacity = 1;
-    var flashFunc = function() {
-      context.fillStyle = "rgba("+[r,g,b,opacity].join(',')+")";
-      context.strokeStyle = "rgba("+[0,0,0,opacity].join(',')+")";
-      context.fillText(text, x, y);
-      context.strokeText(text, x, y);
-      y--;
-      opacity -= .04;
-      if (opacity > 0) {
-        requestAnimationFrame(flashFunc);
-      }
-    }
-    requestAnimationFrame(flashFunc);
-  }
-
-  function processGame() {
-    //var speed = 10;
-    //var maxSpeed = 10;
-    //x += (speed / 2) - Math.random() * speed;
-    //y += (speed / 2) - Math.random() * speed;
-    //if (x < 0) { x = 0 }
-    //if (y < 0) { y = 0 }
-    //if (x > canvas.width) { x = canvas.width }
-    //if (y > canvas.height) { y = canvas.height }
-    var speedVariance = 1;
-    var maxSpeed = 1;
-    xSpeed += (speedVariance / 2) - (Math.random() * speedVariance);
-    ySpeed += (speedVariance / 2) - (Math.random() * speedVariance);
-    xSpeed = clamp(xSpeed, -1 * maxSpeed, maxSpeed);
-    ySpeed = clamp(ySpeed, -1 * maxSpeed, maxSpeed);
-
-    x += xSpeed;
-    y += ySpeed;
-    if (x < 0 || x > canvas.width) {
-      xSpeed *= -1;
-      x = clamp(x, 0, canvas.width);
-    }
-    if (y < 0 || y > canvas.height) {
-      ySpeed *= -1;
-      y = clamp(y, 0, canvas.height);
-    }
-  }
-
-  function clamp(num, min, max) {
-    return Math.min(Math.max(num, min), max);
-  };
-  function distance(x1, y1, x2, y2) {
-    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+container.addEventListener("click", function() {
+  var mouseX = event.x - container.offsetLeft;
+  var mouseY = event.y - container.offsetTop;
+  if (distance(ballX, ballY, mouseX, mouseY) < radius) {
+    texts.push({text: "YAY", x: mouseX, y: mouseY, r: 0, g: 255, b: 0, opacity: 1})
+  } else {
+    texts.push({text: "BOO", x: mouseX, y: mouseY, r: 255, g: 0, b: 0, opacity: 1})
   }
 });
 
-  //if (window.devicePixelRatio) {
-  //  $(canvas).css('height', canvas.height);
-  //  $(canvas).css('width', canvas.width);
-  //  canvas.height *= window.devicePixelRatio;
-  //  canvas.width *= window.devicePixelRatio;
-  //  context.scale(window.devicePixelRatio, window.devicePixelRatio);
-  //}
+requestAnimationFrame(gameLoop);
 
+function gameLoop() {
+  drawBall();
+  drawText();
+  processGame();
+  requestAnimationFrame(gameLoop);
+}
+
+function drawBall() {
+  ballCtx.fillStyle = "rgba(223, 239, 255, 0.1)";
+  ballCtx.fillRect(0, 0, WIDTH, HEIGHT);
+  ballCtx.beginPath();
+  ballCtx.arc(ballX, ballY, radius, 0, 2 * Math.PI, false);
+  ballCtx.fillStyle = 'green';
+  ballCtx.fill();
+  ballCtx.lineWidth = 2;
+  ballCtx.strokeStyle = '#030';
+  ballCtx.stroke();
+}
+
+function drawText(text, x, y, r, g, b) {
+  effectsCtx.clearRect(0, 0, WIDTH, HEIGHT);
+  for (var i = 0; i < texts.length; i++) {
+    var t = texts[i];
+    effectsCtx.fillStyle = "rgba("+[t.r,t.g,t.b,t.opacity].join(',')+")";
+    effectsCtx.strokeStyle = "rgba("+[0,0,0,t.opacity].join(',')+")";
+    effectsCtx.fillText(t.text, t.x, t.y);
+    effectsCtx.strokeText(t.text, t.x, t.y);
+    t.y--;
+    t.opacity -= .04;
+    if (t.opacity <= 0) {
+      texts.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+function processGame() {
+  var speedVariance = 1;
+  var maxSpeed = 1;
+  xSpeed += (speedVariance / 2) - (Math.random() * speedVariance);
+  ySpeed += (speedVariance / 2) - (Math.random() * speedVariance);
+  xSpeed = clamp(xSpeed, -1 * maxSpeed, maxSpeed);
+  ySpeed = clamp(ySpeed, -1 * maxSpeed, maxSpeed);
+
+  ballX += xSpeed;
+  ballY += ySpeed;
+  if (ballX < 0 || ballX > WIDTH) {
+    xSpeed *= -1;
+    ballX = clamp(ballX, 0, WIDTH);
+  }
+  if (ballY < 0 || ballY > HEIGHT) {
+    ySpeed *= -1;
+    ballY = clamp(ballY, 0, HEIGHT);
+  }
+}
+
+function clamp(num, min, max) {
+  return Math.min(Math.max(num, min), max);
+};
+
+function distance(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}
