8e3ebca8512f18290405e4eda34eb0cf6843aa5d
James Gary <mrjamesgary@gmail.com>
2014-08-14 02:52:29 -0700

    Better animation

--- game.js
+++ game.js
@@ -8,6 +8,8 @@ var ctx = canvas.getContext("2d");
 
 var ballX = 200;
 var ballY = 150;
+var xSpeed = 0;
+var ySpeed = 0;
 var radius = 25;
 
 gameLoop();
@@ -32,11 +34,29 @@ function drawBall() {
 }
 
 function moveBall() {
-  var speed = 10;
-  ballX += (speed / 2) - Math.random() * speed;
-  ballY += (speed / 2) - Math.random() * speed;
-  ballX = clamp(ballX, 0, WIDTH);
-  ballY = clamp(ballY, 0, HEIGHT);
+  //var speed = 10;
+  //ballX += (speed / 2) - Math.random() * speed;
+  //ballY += (speed / 2) - Math.random() * speed;
+  //ballX = clamp(ballX, 0, WIDTH);
+  //ballY = clamp(ballY, 0, HEIGHT);
+
+  var speedVariance = 1;
+  var maxSpeed = 5;
+  xSpeed += (speedVariance / 2) - (Math.random() * speedVariance);
+  ySpeed += (speedVariance / 2) - (Math.random() * speedVariance);
+  xSpeed = clamp(xSpeed, -1 * maxSpeed, maxSpeed);
+  ySpeed = clamp(ySpeed, -1 * maxSpeed, maxSpeed);
+
+  ballX += xSpeed;
+  ballY += ySpeed;
+  if (ballX < 0 || ballX > WIDTH) {
+    ballX = clamp(ballX, 0, WIDTH);
+    xSpeed *= -1;
+  }
+  if (ballY < 0 || ballY > HEIGHT) {
+    ballY = clamp(ballY, 0, HEIGHT);
+    ySpeed *= -1;
+  }
 }
 
 function clamp(num, min, max) {
